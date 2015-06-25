7db125346d3f2e3741f88005e683ab1363999438
Guojing Tang <guojingtang.1988@gmail.com>
2015-06-26 00:21:42 +0800

    Test

--- handlers/index.go
+++ handlers/index.go
@@ -1,11 +1,32 @@
 package handlers
 
 import (
+    "fmt"
     "net/http"
     "html/template"
 )
 
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
-    t, _ := template.ParseFiles("templates/index.html")
-    t.Execute(w, nil)
+    switch r.Method {
+       case "GET":
+           // Serve the resource.
+       case "POST":
+           r.ParseMultipartForm(32 << 20)
+           file, handler, err := r.FormFile("uploadfile")
+           if err != nil {
+               fmt.Println(err)
+               return
+           }
+           defer file.Close()
+           fmt.Println(file.name)
+           fmt.Fprintf(w, "%s", file.name)
+           break
+       case "PUT":
+           // Update an existing record.
+       case "DELETE":
+           // Remove the record.
+       default:
+           t, _ := template.ParseFiles("templates/index.html")
+           t.Execute(w, nil)
+    }
 }
